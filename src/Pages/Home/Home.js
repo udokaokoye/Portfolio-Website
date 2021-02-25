89f9defef9e404c4df93ec53a57eebcecbbd2177
udokaokoye <leviokoye@gmail.com>
2021-02-25 16:02:29 +0100

    git commit -m "Change Made @4:02pm"

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import './Home.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+//   faCopyright,
+//   faAngleUp,
+//   faBars,
+//   faTimes,
+// } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookF,
+  faInstagram,
+  faTwitter,
+  faLinkedinIn
+} from "@fortawesome/free-brands-svg-icons";
 const Home = () => {
     return (
         <div>
             <div className="main-home">
                 <div className="show">
                     <div className="left">
-                        <div className="social-links"></div>
-                        <div className="my-det"></div>
+                        <div className="social-links">
+                            <a href=""><FontAwesomeIcon className="fb" color="#fff" icon={faFacebookF} /></a>
+                            <a href=""><FontAwesomeIcon className="tw" color="#fff" icon={faTwitter} /></a>
+                            <a href=""><FontAwesomeIcon className="ig" color="#fff" icon={faInstagram} /></a>
+                            <a href=""><FontAwesomeIcon className="li" color="#fff" icon={faLinkedinIn} /></a>
+                        </div>
+                        <div className="my-det">
+                            <h3>I am Okoye Udoka</h3>
+                            <p>I'm Okoye Udoka A Freelance Web Designer & Developer based in Lagos, Nigeria.
+                                Highly experienced in designing & developing websites.</p>
+                        </div>
                         <div className="show-btns"></div>
                     </div>
                     <div className="right"></div>
