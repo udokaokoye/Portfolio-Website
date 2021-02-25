5e5e29209adbbac1756a6b3098d6892fe7c7f9f0
udokaokoye <leviokoye@gmail.com>
2021-02-25 15:19:00 +0100

    git push -u origin main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
 
+import Home from './Pages/Home/Home';
+import NavBar from './Components/Nav/Nav';
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
+    <Router>
+      <React.Fragment>
+        {/* <NavBar /> */}
+        <Route key={'/'} exact path={'/'}>
+          <Home />
+        </Route>
+      </React.Fragment>
+    </Router>
   );
 }
 
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div>
+            <nav>
+                <div className="logo">
+                    <h1>Okoye Udoka</h1>
+                </div>
+
+                <div className="links"></div>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
