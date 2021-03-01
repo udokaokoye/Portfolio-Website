fb7df8c4a9303e82e7c22f33305a99ff2b49f2a2
udokaokoye <leviokoye@gmail.com>
2021-03-01 17:05:03 +0100

    Added Image To Show

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -15,50 +15,116 @@ const Home = () => {
     return (
         <div>
             <div className="main-home">
-            {/* <Particles id="particles-js" 
+            <Particles id="particles-js" 
             params={{
-                particles: {
-                    number: {
-                        value: 100,
-                        density: {
-                            enable: true,
-                            value_area: 1000
+                "particles": {
+                    "number": {
+                        "value": 150,
+                        "density": {
+                            "enable": true,
+                            "value_area": 1803.4120608655228
                         }
                     },
-                    color: {
-                        value: '#fff'
+                    "color": {
+                        "value": "#ffffff"
                     },
-                    opacity: {
-                        value: 0.5,
-                        anim: {
-                            enable: true
+                    "shape": {
+                        "type": "circle",
+                        "stroke": {
+                            "width": 2,
+                            "color": "#000000"
+                        },
+                        "polygon": {
+                            "nb_sides": 4
+                        },
+                        "image": {
+                            "src": "img/github.svg",
+                            "width": 100,
+                            "height": 100
                         }
                     },
-                    size: {
-                        value: 1,
-                        random: true,
-                        anim: {
-                            enable: true,
-                            speed: 10
+                    "opacity": {
+                        "value": 0.4008530152163807,
+                        "random": false,
+                        "anim": {
+                            "enable": false,
+                            "speed": 1,
+                            "opacity_min": 0.1,
+                            "sync": false
                         }
                     },
-                    line_linked: {
-                        enable: true
+                    "size": {
+                        "value": 1.5,
+                        "random": true,
+                        "anim": {
+                            "enable": false,
+                            "speed": 40,
+                            "size_min": 0.1,
+                            "sync": false
+                        }
                     },
-                    move: {
-                        speed: 0.9
+                    "line_linked": {
+                        "enable": true,
+                        "distance": 100,
+                        "color": "#ffffff",
                     },
-                    
-                 },
-                
-                 interactivity: {
-                    onhover: {
-                        enable: true,
-                        mode: "repulse"
+                    "move": {
+                        "enable": true,
+                        "speed": 6,
+                        "direction": "none",
+                        "random": false,
+                        "straight": false,
+                        "out_mode": "out",
+                        "bounce": false,
+                        "attract": {
+                            "enable": false,
+                            "rotateX": 600,
+                            "rotateY": 1200
+                        }
+                    }
+                },
+                "interactivity": {
+                    "detect_on": "window",
+                    "events": {
+                        "onhover": {
+                            "enable": true,
+                            "mode": "repulse"
+                        },
+                        "onclick": {
+                            "enable": false,
+                            "mode": "bubble"
+                        },
+                        "resize": true
                     },
-                 }
-            }} 
-             /> */}
+                    "modes": {
+                        "grab": {
+                            "distance": 400,
+                            "line_linked": {
+                                "opacity": 1
+                            }
+                        },
+                        "bubble": {
+                            "distance": 400,
+                            "size": 40,
+                            "duration": 2,
+                            "opacity": 8,
+                            "speed": 3
+                        },
+                        "repulse": {
+                            "distance": 100,
+                            "duration": 0.4
+                        },
+                        "push": {
+                            "particles_nb": 4
+                        },
+                        "remove": {
+                            "particles_nb": 2
+                        }
+                    }
+                },
+                "retina_detect": true
+            }}
+             />
              
                 <div className="show">
                     <div className="left">
@@ -77,7 +143,9 @@ const Home = () => {
                            <button>Veiw My Portfolio <span><FontAwesomeIcon className="arrwrgt" color="#fff" icon={faArrowRight} /></span></button> 
                         </div>
                     </div>
-                    <div className="right"></div>
+                    <div className="right">
+                        {/* <img src={mypic} alt=""/> */}
+                    </div>
                 </div>
             </div>
 
