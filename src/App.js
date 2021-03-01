67460945e9bb80a97672a7c2bd64d6cbafd53023
udokaokoye <leviokoye@gmail.com>
2021-03-01 16:17:08 +0100

    added particle.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
   Link,
   NavLink,
 } from "react-router-dom";
-
+import Particles from 'react-particles-js';
 import Home from './Pages/Home/Home';
 import NavBar from './Components/Nav/Nav';
 function App() {
@@ -14,6 +14,18 @@ function App() {
       <React.Fragment>
         <NavBar />
         <Route key={'/'} exact path={'/'}>
+        {/* <Particles width='100%' height='100%' /> */}
+        {/* <Particles 
+                params={{
+                    polygon: {
+                        enable: true,
+                        type: 'inside',
+                        move: {
+                            radius: 10
+                        },
+                        // url: 'path/to/svg.svg'
+                    }
+                }} /> */}
           <Home />
         </Route>
       </React.Fragment>
