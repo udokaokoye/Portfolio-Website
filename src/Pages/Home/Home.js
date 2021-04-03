23c398e2302b85d03c856374d74c88dc79e8ec66
Udoka Okoye <leviokoye@gmail.com>
2021-04-03 18:43:43 +0100

    Add files via upload

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Home.css';
 import Particles from 'react-particles-js';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import Isotope from 'isotope-layout';
+// import $ from 'jquery';
 import {
   faArrowRight,
   faTachometerAlt,
@@ -58,6 +60,45 @@ const Home = () => {
             percent: 50
         },
     ];
+
+// const [filterKey, setFilterKey] = React.useState('*');
+
+// const projects = [
+//     {
+//         name: 'All Saints AHQ Cathedral',
+//         tools: 'React Js / PHP',
+//         category: 'react js'
+//     },
+
+//     {
+//         name: 'Crypto lion x2',
+//         tools: 'PHP',
+//         category: 'php'
+        
+//     },
+// ];
+
+// const [projects_arr, setprojects_arr] = useState(projects);
+
+
+
+// const tools = ['react', 'python', 'react naive', 'php',];
+
+
+    // const filterTools = (filterKey) => {
+    //     // projects.filter((project) => {
+    //     //     return(project.category == 'react')
+    //     // })
+
+
+    //     projects.map((prjc) => {
+    //         return prjc.category === 'react';
+    //     })
+    // }
+
+    // console.log(filterTools())
+
+
     return (
         <div>
             <div id='home' className="main-home">
@@ -331,21 +372,27 @@ Let's make something special</p>
            <div className="right">
                <div className="bar">
                    <div className="br1 active_br"><span>All</span></div>
-                   <div className="br2"><span>React Js</span></div>
+                   <div
+                //    onClick={() => {filterTools('react')}}
+                    // onClick={handleFilterKeyChange('react')} 
+                    className="br2"><span>React Js</span></div>
                    <div className="br3"><span>React Native</span></div>
                    <div className="br4"><span>PHP</span></div>
                    <div className="br5"><span>Python</span></div>
                </div>
 
-               <div className="prjs">
-                   <div className="project_div prj1">
-                       <div className="prj_card">
-                       <div className="prj_txt"><span>All Saints AHQ Cathedral</span></div>
-                       <div className="prj_but"><button>Learn More</button></div>
-                       </div>
-                       
-                   </div>
-                   <div className="project_div prj2"></div>
+               <div className="prjs" id="prjs">
+
+                   
+                        <div className="project_div prj1">
+                        <div className="prj_card">
+                        <div className="prj_txt"><span>All Saints AHQ Cathedral<br/> <span className='lng_used'>React Js / PHP</span></span></div>
+                        <div className="prj_but"><button>Learn More</button></div>
+                        </div>
+                        
+                        </div>
+                   
+                   <div className="project_div prj2 react"></div>
                    <div className="project_div prj3"></div>
                    <div className="project_div prj4"></div>
                    <div className="project_div prj5"></div>
@@ -354,7 +401,11 @@ Let's make something special</p>
            </div>
            </div>
         </div>
+
+                        {/* <div className="prjss"></div>
+        <div className="react"></div> */}
         </div>
+        
     )
 }
 
